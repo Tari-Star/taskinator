49a7e5299485b86b9bc2342acbd091af4bccded0
Tari <bota.maral@gmail.com>
2021-09-09 16:42:48 -0400

    save tasks to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,10 +62,11 @@ var createTaskEl = function (taskDataObj) {
 
   taskDataObj.id = taskIdCounter;
   tasks.push(taskDataObj);
+
+  saveTasks();
   // increase task counter for next unique id
   taskIdCounter++;
-  console.log(taskDataObj);
-  console.log(taskDataObj.status);
+  
 };
 
 var createTaskActions = function (taskId) {
@@ -127,6 +128,8 @@ var completeEditTask = function (taskName, taskType, taskId) {
 
   alert("Task Updated!");
 
+  saveTasks();
+
   // remove data attribute from form
   formEl.removeAttribute("data-task-id");
   // update formEl button to go back to saying "Add Task" instead of "Edit Task"
@@ -175,7 +178,7 @@ var taskStatusChangeHandler = function (event) {
            tasks[i].status = statusValue;
        }
    }
-   console.log(tasks);
+   saveTasks();
 };
 
 var editTask = function (taskId) {
@@ -223,8 +226,12 @@ var deleteTask = function (taskId) {
   }
    // reassign tasks array to be the same as updatedTaskArr
    tasks = updatedTaskArr;
+   saveTasks();
 };
 
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
 
